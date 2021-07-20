607f6febea2d22a9941d12619fc89ebbb4fde8ea
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-07-19 20:46:59 -0700

    add test.js

---
+++ api/routes/test.js
@@ -0,0 +1,13 @@
+//const express = require("express");
+//const router = express.Router();
+
+export default (req, res) => {
+    res.end('Test API!')
+}
+/*
+router.use('/test', (req, res) => {
+    res.end('Test API!')
+})
+
+module.exports = router
+*/
